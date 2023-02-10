09a9326798d84d78ac0d8290f8afb3a3fe33cb00
BrentRip <btorregosa15@gmail.com>
2023-02-10 16:20:41 -0500

    v3

---
+++ capstone/src/pages/components/signupform.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+//form for sign up
+//will add this user to the database and the ability to login
+const Signupform = () => {
+  //res.status(200)
+  return (
+    <form onSubmit="signuphandler" name="signup">
+        <h1>Sign up</h1>
+        <div>
+          <label>Enter First Name: <input type="text" /> </label>
+          <label>Enter Last Name: <input type="text" /> </label>
+        </div>
+        
+        <label>Enter Email:</label>
+          <input type="text"></input>
+        <label>Enter Date of Birth: <input type="text" /> </label>
+        <label>Enter Address:</label>
+         <input type="text"></input>
+        <label>Enter Phone Number: <input type="text" /> </label>
+        
+        {/* login information */}
+        <label>Enter Username: <input type="text" /> </label>
+        <label>Enter Password: <input type="password" /> </label>
+        <label>Re-Enter Password: <input type="password" /> </label>
+
+      <div>
+        <input type='submit' name='Sign Up'></input>
+      </div>
+      <div>
+        <label>Already have an account?</label>
+      </div>
+    </form>
+    
+  )
+};
+export default Signupform;
+// const root = ReactDom.createRoot(document.getElementById('root'));
+// root.render(<signupform/>);
+
