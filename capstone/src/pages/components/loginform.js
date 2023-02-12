d0255d126d10a549ea82e921018795c0cd7cf1ea
BrentRip <btorregosa15@gmail.com>
2023-02-12 14:11:00 -0500

    signup and login completed

--- capstone/src/pages/components/loginform.js
+++ capstone/src/pages/components/loginform.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
-function loginform() {
-  return (
-    <div>
-      
-    </div>
-  )
+const viewpassword =() => {
+  var x = document.getElementById('passinput');
+  if(x.type === 'password') {
+    x.type = 'text';
+  }
+  else {
+    x.type = 'password'
+  }
 }
-
-export default loginform
+const Loginform = () => {
+  return (
+    <form onSubmit="loginhandler" method='POST' name="login">
+        
+        <div className="login">
+          <h1>Login to FIT</h1>
+          {/* login information */}
+          <input type='text' placeholder='Username' id='usernameinput'/>
+          
+          <input type="password" placeholder='Password' id='passinput'/>
+          <br></br>
+          <input type='checkbox' onClick={()=>viewpassword()}/>Show Password 
+          <br></br>
+          <input type='submit' name='Sign Up'></input>
+        </div>
+      <label>Don't have an account?</label>
+     
+    </form>
+  )
+};
+export default Loginform;
--- capstone/src/pages/components/signupform.js
+++ capstone/src/pages/components/signupform.js
@@ -1,33 +1,59 @@
 import React from 'react'
-
-
+//import '../pageprops/viewpassword.js'
+import VisibilityIcon from '@mui/icons-material/Visibility';
 //form for sign up
 //will add this user to the database and the ability to login
+const viewpassword =() => {
+    var x = document.getElementsByName('passinput1');
+    var y = document.getElementsByName('passinput2');
+    if(x.type === 'password' || y.type === 'password') {
+      x.type = 'text';
+      y.type = 'text';
+    }
+    else {
+      x.type = 'password'
+      y.type = 'password'
+    }
+}
+const passwordmatch = () => {
+  var x = document.getElementsByName('passinput1');
+  var y = document.getElementsByName('passinput2');
+  if (x == y){
+  }
+  else {
+    alert('Passwords do not match');
+  }
+}
+// const [ re]
 const Signupform = () => {
-  //res.status(200)
   return (
-    <form onSubmit="signuphandler" name="signup">
-        <h1>Sign up</h1>
-              
-        <label>Enter First Name: <input type="text" /> </label>
-        <label>Enter Last Name: <input type="text" /> </label>
+    <form onSubmit="signuphandler" method='POST' name="signup">
         
-        <label>Enter Email:<input type="text" /> </label> 
-        <label>Enter Date of Birth: <input type="text" /> </label>
-        <label>Enter Address:<input type="text"/> </label>
-        <label>Enter Phone Number: <input type="text" /> </label>
+        <div className="signup">
+          <h1>Sign up For A FIT Account</h1>
+          {/* sign up infromation added to the database*/}
+          <input type='text' name='firstname' placeholder='Enter First Name' id='firstnameinput'/>
+          <input type='text' name='lastname' placeholder='Enter Last Name' id='firstlastinput'/>
+          <br></br>
+          <input type='text' name='email' placeholder='Enter Email' id='emailinput'/>
+          <input type='text' name='dob' placeholder='Enter Date Of Birth MM/DD/YYYY' id='dobinput'/>
+          <input type='text' name='address' placeholder='Enter Address' id='addressinput'/>
+          <input type='text' name='' placeholder='Enter Phone Number' id='phonenumberinput'/>
+          <br></br>
+          {/* login information that will pulled later for login*/}
+          <input type='text' placeholder='Username' id='usernameinput'/>
+          <input type="password" placeholder='Password' id='passinput1'/>
+          {/* add password re-enter and match with pasinput1 */}
+          {/* <input type="password" placeholder='Re Enter Password' id='passinput2'/> */}
+          <br></br>
+          <input type='checkbox' onClick={()=>viewpassword()}/>Show Password 
+          <br></br>
+          <input type='submit' name='Sign Up'></input>
+        </div>
         
-        {/* login information */}
-        <label>Enter Username: <input type="text" /> </label>
-        <label>Enter Password: <input type="password" /> </label>
-        <label>Re-Enter Password: <input type="password" /> </label>
-
-      <div>
-        <input type='submit' name='Sign Up'></input>
-      </div>
-      <div>
-        <label>Already have an account?</label>
-      </div>
+      
+      <label>Already have an account?</label>
+     
     </form>
     
   )
