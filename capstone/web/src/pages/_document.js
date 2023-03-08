c50ccef24dc17c74a2e84ddc9b6d47b7a4da3e64
Nostrebor <88103431+Zankix@users.noreply.github.com>
2023-03-08 18:04:02 -0500

    another sexier update

--- capstone/web/src/pages/_document.js
+++ capstone/web/src/pages/_document.js
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <div name = 'main-background'>
       <body>
         <Main />
         <NextScript />
       </body>
+      </div>
     </Html>
   )
 }
