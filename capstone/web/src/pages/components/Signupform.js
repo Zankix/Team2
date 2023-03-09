f9472365ceddc11fa9a12c30659ce233d13c52b7
BrentRip <btorregosa15@gmail.com>
2023-03-08 19:44:30 -0500

    not working but committed cause

--- capstone/web/src/pages/components/Signupform.js
+++ capstone/web/src/pages/components/Signupform.js
@@ -18,30 +18,31 @@ const viewpassword =() => {
       y.type = 'password'
     }
 }
+const clear =() =>{
+  document.getElementById('firstnameinput').value ='';
+  document.getElementById('lastnameinput').value ='';
+  document.getElementById('emailinput').value ='';
+}
 export default function Signupform(){
   const router = useRouter();
-  const [ sfirstname, setfirstname ] = useState('');
-  const [ slastname, setlastname ] = useState('');
-  const [ semail, setemail ] = useState('');
-  const [ sdob, setdob ] = useState('');
-  const [ saddress, setaddress ] = useState('');
-  const [ sphonenumber, setphonenumber ] = useState('');
-  const [ susername, setusername ] = useState('');
-  const [ spassword, setpassword ] = useState('');
-  const [ srepassword, setrepassword ] = useState('');
+  const [ firstname, setfirstname ] = useState('');
+  const [ lastname, setlastname ] = useState('');
+  const [ email, setemail ] = useState('');
+
+  
+  const nextpart = async(event) => {
+    event.preventDefault();
+    const user1 = await pb.collection('user').create(data)
+    router.push('./Signupform2');
+  }
+  
 
   async function adduser() {
     const data = {
-      "username": susername,
-      "email": semail,
+      "email": email,
       "emailVisibility": true,
-      "password": spassword,
-      "passwordConfirm": srepassword,
-      "firstname": sfirstname,
-      "lastname": slastname,
-      "address": saddress,
-      "phonenumber": sphonenumber,
-      "dob": sdob
+      "firstname": firstname,
+      "lastname": lastname,
   };
   try {
     const record = await pb.collection('user').create(data);
@@ -57,27 +58,29 @@ export default function Signupform(){
       <NavBar></NavBar>
         <div name='signup'>
           <h1>Sign up For A FIT Account</h1>
-          {/* sign up infromation added to the database*/}
-          <input type='text' name='firstname' placeholder='Enter First Name' onChange={e => setfirstname(e.target.value)} id='firstnameinput'/>
-          <input type='text' name='lastname' placeholder='Enter Last Name' onChange={e => setlastname(e.target.value)} id='lastnameinput'/>
+          {/* sign up infromation added to the database part 1*/}
+          <label>
+            First Name: 
+           <input type='text' name='firstname' placeholder='Enter First Name' onChange={e => setfirstname(e.target.value)} id='firstnameinput'/>
+          </label>
           <br></br>
-          <input type='text' name='email' placeholder='Enter Email' onChange={e => setemail(e.target.value)} id='emailinput'/>
-          <input type='text' name='dob' placeholder='Enter Date Of Birth MM/DD/YYYY' onChange={e => setdob(e.target.value)} id='dobinput'/>
-          <input type='text' name='address' placeholder='Enter Address' onChange={e => setaddress(e.target.value)} id='addressinput'/>
-          <input type='text' name='phonenumber' placeholder='Enter Phone Number' onChange={e => setphonenumber(e.target.value)} id='phonenumberinput'/>
+          <label>
+            Last Name:
+            <input type='text' name='lastname' placeholder='Enter Last Name' onChange={e => setlastname(e.target.value)} id='lastnameinput'/>
+          </label>
+         <br></br>
+          <label>
+            Email: 
+            <input type='text' name='email' placeholder='Enter Email' onChange={e => setemail(e.target.value)} id='emailinput'/>
+           </label>
+          
           <br></br>
+          <button onClick={clear}>Clear</button>
+          <button onClick={nextpart}>Next</button>
           {/* login information that will pulled later for login*/}
-          <input type='text' name='username' placeholder='Username' onChange={e => setusername(e.target.value)} id='usernameinput'/>
-          <input type="password" name='password' placeholder='Password' onChange={e => setpassword(e.target.value)} id='passinput1'/>
-          {/* add password re-enter and match with pasinput1 */}
-          <input type="password" name='repassword' placeholder='Re Enter Password' onChange={e => setrepassword(e.target.value)} id='passinput2'/>
-          <br></br>
-          <input type='checkbox' onClick={()=>viewpassword()}/>Show Password 
-          <br></br>
-          <button onClick={adduser}>Sign up</button>
+         
           {/* <input type='submit' name='Sign Up' onClick={signuphandler}></input> */}
         </div>
-          <label>Already have an account?</label>
     </div>
     
     
